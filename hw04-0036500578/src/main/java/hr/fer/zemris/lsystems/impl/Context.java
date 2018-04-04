aa3b2f54d10f1d39494f663f9079f55729c07ed4
JosipTrbuscic <josip.trbuscic@fer.hr>
2018-04-04 19:59:43 +0200

    Cetvrti zadatak

--- hw04-0036500578/src/main/java/hr/fer/zemris/lsystems/impl/Context.java
+++ hw04-0036500578/src/main/java/hr/fer/zemris/lsystems/impl/Context.java
@@ -1,24 +1,52 @@
 package hr.fer.zemris.lsystems.impl;
 
+import hr.fer.zemris.java.custom.collections.EmptyStackException;
 import hr.fer.zemris.java.custom.collections.ObjectStack;
 
+/**
+ * Instances of this class are used for storing {@code TurtleState}s.
+ *  States are stored on a stack. Class offers basic stack
+ * operations like push, pop and peek
+ * @author Josip Trbuscic
+ *
+ */
 public class Context {
+	/**
+	 * Stack in which {@code TurtleState}s are stored 
+	 */
 	private ObjectStack stack;
 	
+	/**
+	 * Constructs new {@code ObjectStack} with default size
+	 */
 	public Context() {
 		stack = new ObjectStack();
 	}
 	
+	/**
+	 * Returns {@code TurtleState} from top of the stack
+	 * without removing it
+	 * @return {@code TurtleState} from top of a stack
+	 * @throws EmptyStackException if stack is empty
+	 */
 	public TurtleState getCurrentState() {
 		return (TurtleState) stack.peek();
 	}
 	
+	/**
+	 * Adds given {@code TurtleState} to the top of the stack
+	 * @param state which will be added
+	 * @throws NullPointerException if given argument is null
+	 */
 	public void pushState(TurtleState state) {
 		if(state == null) throw new NullPointerException("Turtle state cannot be null");
 		
 		stack.push(state);
 	}
 	
+	/**
+	 * Removes the first {@code TurtleState} from top of the stack.
+	 */
 	public void popState() {
 		stack.pop();
 	}
--- hw04-0036500578/src/main/java/hr/fer/zemris/math/Vector2D.java
+++ hw04-0036500578/src/main/java/hr/fer/zemris/math/Vector2D.java
@@ -1,25 +1,59 @@
 package hr.fer.zemris.math;
 
-import java.util.Vector;
-
+/**
+ * Representation of two dimensional vector with x and y components. Class
+ * offers operations to translate, rotate and scale vectors.
+ * 
+ * @author Josip Trbuscic
+ *
+ */
 public class Vector2D {
+	
+	/**
+	 * Threshold used for comparing vectors
+	 */
 	public static final double DELTA = 1E-6;
+	
+	/**
+	 * Abscissa coordinate
+	 */
 	private double x;
+	
+	/**
+	 * Ordinate coordinate
+	 */
 	private double y;
 
+	/**
+	 * Constructs new {@code Vector2D} out of given coordinates
+	 * @param x coordinate
+	 * @param y coordinate
+	 */
 	public Vector2D(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-
+	
+	/**
+	 * @return x coordinate
+	 */
 	public double getX() {
 		return x;
 	}
-
+	
+	/**
+	 * @return y coordinate
+	 */
 	public double getY() {
 		return y;
 	}
 
+	/**
+	 * Translates point represented by x and y coordinate of this vector
+	 * in a direction specified by given {@code Vector2D} 
+	 * @param offset - distance and direction of translation
+	 * @throws NullPointerException if given argument is null
+	 */
 	public void translate(Vector2D offset) {
 		if (offset == null)
 			throw new NullPointerException("Offset cannot be null");
@@ -28,6 +62,15 @@ public class Vector2D {
 		y += offset.y;
 	}
 
+
+	/**
+	 * Creates new {@code Vector2D} as a copy of a current one and
+	 * translates it direction specified by given {@code Vector2D}.
+	 * Original vector remains unchanged.
+	 * @param offset - distance and direction of translation
+	 * @return new translated vector
+	 * @throws NullPointerException if given argument is null
+	 */
 	public Vector2D translated(Vector2D offset) {
 		Vector2D translated = copy();
 		translated.translate(offset);
@@ -35,63 +78,107 @@ public class Vector2D {
 		return translated;
 	}
 
+	/**
+	 * Rotates the vector around the origin of a coordinate system 
+	 * by given angle. Angle must be given as a value in degrees. 
+	 * @param angle which specifies rotation
+	 */
 	public void rotate(double angle) {
 		double radians = angle * Math.PI / 180;
 
 		double rotatedX = x * Math.cos(radians) - y * Math.sin(radians);
 		double rotatedY = x * Math.sin(radians) + y * Math.cos(radians);
-	
+
 		x = rotatedX;
 		y = rotatedY;
 	}
-	
+
+	/**
+	 * Returns new instance of {@code Vector2D} rotated
+	 * by given angle. Angle must be given as a value in degrees.
+	 * Original vector remains unchanged.
+	 * @param angle which specifies rotation
+	 * @return new rotated vector
+	 */
 	public Vector2D rotated(double angle) {
 		Vector2D rotated = copy();
 		rotated.rotate(angle);
-		
+
 		return rotated;
 	}
-	
+
+	/**
+	 * Scales vector with given value
+	 * @param scale - value to scale with 
+	 */
 	public void scale(double scale) {
-		x*=scale;
-		y*=scale;
+		x *= scale;
+		y *= scale;
 	}
-	
+
+	/**
+	 * Returns new instance of {@code Vector2D} scaled
+	 * with given value. Original vector remains unchanged.
+	 * @param scale - value to scale with
+	 * @return new scaled vector
+	 */
 	public Vector2D scaled(double scale) {
 		Vector2D scaled = copy();
 		scaled.scale(scale);
-		
+
 		return scaled;
-		
+
 	}
 	
+	/**
+	 *Returns new instance of {@code Vector2D} with same 
+	 *parameters as current one 
+	 * @return copy of a current vector
+	 */
 	public Vector2D copy() {
 		return new Vector2D(x, y);
 	}
-	
+
+	/**
+	 * Returns angle which this vector encloses with 
+	 * positive part of x axis
+	 * @return angle with positive part of x axis
+	 */
 	public double getAngle() {
 		return Math.atan2(y, x);
 	}
-	
+
+	/**
+	 * Returns {@code String} representation of vector
+	 * @return {@code String} representation of a vector
+	 */
 	@Override
 	public String toString() {
-		if(y<0) {
-			return String.format("%.2fi - %.2fj", x,-y);
+		if (y < 0) {
+			return String.format("%.2fi - %.2fj", x, -y);
 		} else {
-			return String.format("%.2fi + %.2fj", x,y);
+			return String.format("%.2fi + %.2fj", x, y);
 		}
 	}
-	
+
+	/**
+	 * Compares {@code Vector2D} with given object
+	 * @return {@code true} if objects are equal, {@code false} otherwise
+	 */
 	@Override
 	public boolean equals(Object o) {
-		if(o == null) return false;
-		if(!(o instanceof Vector2D)) return false;
-		
+		if (o == null)
+			return false;
+		if (!(o instanceof Vector2D))
+			return false;
+
 		Vector2D v = (Vector2D) o;
-		
-		if(Math.abs(v.x-this.x)>DELTA) return false;
-		if(Math.abs(v.y-this.y)>DELTA) return false;
-		
+
+		if (Math.abs(v.x - this.x) > DELTA)
+			return false;
+		if (Math.abs(v.y - this.y) > DELTA)
+			return false;
+
 		return true;
 	}
 }
